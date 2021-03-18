2735d30ab54539a509c4a6ad3eae69a31d60b4b1
baha2046a <baha2046@yahoo.co.jp>
2021-03-18 08:07:10 +0000

    A

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :users
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
